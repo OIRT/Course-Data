f2d38773fb3c7561dbc5544a4ac7b1d26cf88e76
Eric Jeney <emjeney@gmail.com>
2012-06-22 09:53:12 -0400

    Add support for static files (CSS and SASS)

--- src/course_data/data_app/views.py
+++ src/course_data/data_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def index(request):
-    return render_to_response('data_app/index.html', {})
+    return render_to_response('data_app/index.html', {}, context_instance=RequestContext(request))
