2aa298671480c9be130eeb6f78b303757fef2c20
Eric Jeney <emjeney@gmail.com>
2012-06-28 13:58:15 -0400

    Add preliminary models for User and Course.

--- src/course_data/data_app/models.py
+++ src/course_data/data_app/models.py
@@ -1,5 +1,33 @@
-from django.db import models
+from mongoengine import *
 
-class CourseEntry(models.Model):
-    courseNumber = models.IntegerField()
-    studentName = models.TextField()
+
+# Not implemented in the database yet.
+class Student(EmbeddedDocument):
+    student_num = IntField()
+    graduation_year = IntField(4)
+    recent_math_course = StringField()
+    highest_math_course = StringField()
+    iclicker_serial = IntField()
+
+
+class Course(EmbeddedDocument):
+    id = StringField()
+
+    # Necessary so that MongoEngine doesn't require all sorts
+    # of extra weird fields to be there.
+    meta = {'allow_inheritance': False}
+
+
+class User(Document):
+    firstname = StringField()
+    lastname = StringField()
+    nickname = StringField()
+    email = EmailField()
+    roles = ListField(StringField())
+    ruid = StringField()
+    netid = StringField()
+    rcpid = IntField()
+    courses = ListField(EmbeddedDocumentField(Course))
+    student_data = EmbeddedDocumentField(Student)
+
+    meta = {'allow_inheritance': False}
