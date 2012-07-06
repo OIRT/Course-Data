ed1d9ce44f879a8d4d41db4fed4045e82233f836
Catherine Weresow <weresow@rutgers.edu>
2012-07-06 12:18:31 -0400

    Added documents_to_json()

--- src/course_data/data_app/mongohelpers.py
+++ src/course_data/data_app/mongohelpers.py
@@ -1,5 +1,8 @@
+from mongoengine.queryset import QuerySet
+from mongoengine import Document, ObjectIdField
 from django.http import Http404
-
+import json
+from bson import json_util
 
 def get_document_or_404(klass, *args, **kwargs):
         try:
@@ -7,3 +10,18 @@ def get_document_or_404(klass, *args, **kwargs):
         except klass.DoesNotExist:
                 raise Http404('Document does not exist.')
 
+def documents_to_json(docs, **kwargs):
+    """ Takes a mongoengine document or QuerySet, returns json.
+    
+    Extra arguments are passed on to json.dumps()
+    
+    """
+    if isinstance(docs, QuerySet):
+        jsonlist = []
+        for d in docs:
+            jsonlist.append(documents_to_json(d, **kwargs))
+        jsonstr = "[ %s ]" % ",".join(jsonlist)
+    else:
+        jsonstr = json.dumps(docs.to_mongo(), default=json_util.default, **kwargs)
+    return jsonstr
+    
