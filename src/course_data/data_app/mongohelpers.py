c0ac13fff69fbacfd0f8b40172675e4378b31389
Catherine Weresow <weresow@rutgers.edu>
2012-07-09 16:52:02 -0400

    Update documents_to_json() to accept lists as well as QuerySets, and to use brackets=False to leave out the surrounding []

--- src/course_data/data_app/mongohelpers.py
+++ src/course_data/data_app/mongohelpers.py
@@ -10,17 +10,20 @@ def get_document_or_404(klass, *args, **kwargs):
         except klass.DoesNotExist:
                 raise Http404('Document does not exist.')
 
-def documents_to_json(docs, **kwargs):
-    """ Takes a mongoengine document or QuerySet, returns json.
-    
+def documents_to_json(docs, brackets=True, **kwargs):
+    """ Takes a mongoengine document or QuerySet/list of documents, returns json.
+    Use brackets=False to leave out the outer "[ ]", useful for appending different lists
     Extra arguments are passed on to json.dumps()
     
     """
-    if isinstance(docs, QuerySet):
+    if isinstance(docs, QuerySet) or isinstance(docs, list):
         jsonlist = []
         for d in docs:
             jsonlist.append(documents_to_json(d, **kwargs))
-        jsonstr = "[ %s ]" % ",".join(jsonlist)
+        if brackets:
+            jsonstr = "[ %s ]" % ",".join(jsonlist)
+        else:
+            jsonstr = ",".join(jsonlist)
     else:
         jsonstr = json.dumps(docs.to_mongo(), default=json_util.default, **kwargs)
     return jsonstr
