332dc96c5bed3843a780e00afb01eb3a572fd78c
Eric Jeney <emjeney@gmail.com>
2012-07-16 11:10:38 -0400

    Update the javascript so that it loads all of the data via Ajax from a JSON file.

--- src/course_data/data_app/static/data_app/js/query.js
+++ src/course_data/data_app/static/data_app/js/query.js
@@ -49,255 +49,271 @@ function updateColumnVisibility() {
     });
 }
 
-$(document).ready(function() {
-    $("#result").load("/users", function() {
-        masterDataTable = $("#userTable").dataTable({
-            "bJQueryUI": true,
-            "sDom": 'R<"H"lr>t<"F"ip>',
-            "bScrollXInner": true,
-            "sScrollX": "100%",
-            "bScrollAutoCss": true
-        });
+function tableInitialized() {
+    $.fn.dataTableExt.afnFiltering.push(
+        function(oSettings, aData, iDataIndex) {
+            calculateIndexNums();
+            andVor = $("#andVor").val() === "all";
 
-        $.fn.dataTableExt.afnFiltering.push(
-            function(oSettings, aData, iDataIndex) {
-                calculateIndexNums();
-                andVor = $("#andVor").val() === "all";
-
-                if(andVor) {
-                    show = true;
-                }else {
+            if(andVor) {
+                show = true;
+            }else {
+                show = false;
+            }
+            $("#filterDivContainer").find(".filterDiv").each(function() {
+                r = checkCondition(oSettings, aData, iDataIndex, $(this));
+                if(!r && andVor) {
                     show = false;
+                    return;
+                }else if(r && !andVor) {
+                    show = true;
+                    return;
                 }
-                $("#filterDivContainer").find(".filterDiv").each(function() {
-                    r = checkCondition(oSettings, aData, iDataIndex, $(this));
-                    if(!r && andVor) {
-                        show = false;
-                        return;
-                    }else if(r && !andVor) {
-                        show = true;
-                        return;
-                    }
-                });
+            });
 
-                return show;
-            }
-        );
+            return show;
+        }
+    );
 
-        FiltersControl = can.Control({
-            init: function(element, options) {
-                this.element.html(can.view('static/data_app/views/filterList.ejs', {
-                    filters: this.options.filters
-                }));
-                $(".filterSelect").ufd();
-            },
-
-            '.notOperator change': function(el, ev) {
-                var index = this.findIndex(el);
-                this.options.filters[index].attr("not", (el.val() === "!"));
-            },
-
-            '.ufd select change': function(el, ev) {
-                var index = this.findIndex(el);
-                this.options.filters[index].attr("selection", el.val());
-            },
-
-            '.filterOperator change': function(el, ev) {
-                var index = this.findIndex(el);
-                this.options.filters[index].attr("operator", el.val());
-            },
-
-            '.filterText keyup': function(el, ev) {
-                if(ev.keyCode == 13) {
-                    masterDataTable.fnStandingRedraw();
-                } else {
-                    var index = this.findIndex(el);
-                    this.options.filters[index].attr("text", el.val());
-                }
-            },
-
-            '.deleteFilter click': function(el, ev) {
-                var index = this.findIndex(el);
-                this.options.filters.splice(index, 1);
-                $(".filterSelect").ufd();
-                masterDataTable.fnStandingRedraw();
-            },
-
-            findIndex: function(el) {
-                return $('.filterItem').index(el.closest('.filterItem'));
-            }
-        });
-
-        AndVOrControl  = can.Control({
-            init: function(element, options) {
-                this.element.html(can.view('static/data_app/views/andVor.ejs', {
-                    andVor: this.options.andVor
-                }));
-            },
-
-            '#andVor change': function(el, ev) {
-                this.options.andVor.attr("andVor", el.val());
-            }
-        });
-
-        ColumnsControl = can.Control({
-            // Make sure all of the columns are in the correct order, with correct visibility
-            init: function(element, options) {
-                columns = this.options.columns;
-
-                var oSettings = masterDataTable.fnSettings();
-
-                calculateIndexNums();
-
-                // Goes through and puts all of the visible columns in the correct
-                // order on the left, and the invisible columns on the right.
-                for(i = 0; i < columns.length; i++) {
-                    var from = indexNums[columns[i]];
-                    var to = i;
-                    toTitle = oSettings.aoColumns[to].sTitle;
-
-                    if(from !== to) {
-                        masterDataTable.fnColReorder(from, to);
-
-
-                        // Updates our cache of column order
-                        var min = (to < from)? to : from;
-                        var max = (to > from)? to : from;
-                        for(j = min; j <= max; j++) {
-                            indexNums[oSettings.aoColumns[j].sTitle] = j;
-                        }
-                    }
-                }
-
-                // Hide the columns that aren't mentioned in the column list
-                for(i = 0; i < oSettings.aoColumns.length; i++) {
-                    masterDataTable.fnSetColumnVis(i, (i < columns.length));
-                }
-
-                masterDataTable.bind('column-reorder', this.updateColumns);
-            },
-
-            updateColumns: function() {
-                var oSettings = masterDataTable.fnSettings();
-                var aoColumns = oSettings.aoColumns;
-                var size = aoColumns.length;
-
-                var newColumns = [];
-                for(i = 0; i < size; i++) {
-                    if(aoColumns[i].bVisible) {
-                        newColumns.push(aoColumns[i].sTitle);
-                    }
-                }
-
-                columns.attr(newColumns);
-            }
-        });
-
-        VisControl = can.Control({
-            init: function() {
-                this.element.html(can.view('static/data_app/views/visDialog.ejs', {
-                    columns: masterDataTable.fnSettings().aoColumns
-                }));
-
-                $("#visDialog").dialog({
-                    autoOpen: false,
-                    buttons: {
-                        "Ok": function() {
-                            updateColumnVisibility();
-                            $(this).dialog("close");
-                        }
-                    },
-                    modal: true,
-                    draggable: false,
-                    minWidth: 500,
-                    title: "Show / Hide Columns",
-                    show: { effect: "fade", speed: 1000 },
-                    hide: { effect: "fade", speed: 1000 }
-                });
-            },
-
-            '#visDialogButton click': function() {
-                $("#visDialog").dialog('open');
-            },
-
-            '#visDialog close': function() {
-                calculateIndexNums();
-                $(".colVisCheckbox").each(function() {
-                    masterDataTable.fnSetColumnVis(indexNums[$(this).attr("name")], $(this).attr("checked") === "checked");
-                });
-            }
-        });
-
-        var workspace = {
-            "andVor": "and",
-            "filters": [
-                {
-                    "not": false,
-                    "selection": "Lab 1 Density",
-                    "operator": "<=",
-                    "text": "40"
-                },
-                {
-                    "not": true,
-                    "selection": "Final Exam",
-                    "operator": ">",
-                    "text": "200"
-                }
-            ],
-            "columns": [
-                "lastname",
-                "firstname",
-                "netid",
-                "email",
-                "Lab 1 Density",
-                "Final Exam"
-            ],
-            "sorting": {
-                "column": "lastname",
-                "direction": "asc"
-            }
-        };
-
-        var filters = new can.Observe.List(workspace.filters);
-        var andVor = new can.Observe().attr("andVor", workspace.andVor);
-        var columns = new can.Observe.List(workspace.columns);
-
-        new FiltersControl('#filterDivContainer', {
-            filters: filters
-        });
-
-        new AndVOrControl('#andVorDiv', {
-            andVor: andVor
-        });
-
-        new ColumnsControl('#userTable', {
-            columns: columns
-        });
-
-        new VisControl('#visDialogContainer');
-
-        $("#newFilter").bind("click", function() {
-            filters.push({not: false, "selection": "", operator: "<", "text": ""});
+    FiltersControl = can.Control({
+        init: function(element, options) {
+            this.element.html(can.view('static/data_app/views/filterList.ejs', {
+                filters: this.options.filters
+            }));
             $(".filterSelect").ufd();
-        });
+        },
 
-        // Restore sorting order from workspace
-        calculateIndexNums();
-        var settings = masterDataTable.fnSettings();
-        settings.aaSorting[0][0] = indexNums[workspace.sorting.column];
-        settings.aaSorting[0][1] = workspace.sorting.direction;
-        settings.aaSorting[0][2] = workspace.sorting.direction === "asc"? 0 : 1;
+        '.notOperator change': function(el, ev) {
+            var index = this.findIndex(el);
+            this.options.filters[index].attr("not", (el.val() === "!"));
+        },
 
-        masterDataTable.bind('sort', function() {
-            var sortData = settings.aaSorting[0];
-            if(workspace.sorting.column !== settings.aoColumns[sortData[0]].sTitle ||
-                    workspace.sorting.direction !== sortData[2]) {
-                workspace.sorting.column = settings.aoColumns[sortData[0]].sTitle;
-                workspace.sorting.direction = sortData[2];
+        '.ufd select change': function(el, ev) {
+            var index = this.findIndex(el);
+            this.options.filters[index].attr("selection", el.val());
+        },
+
+        '.filterOperator change': function(el, ev) {
+            var index = this.findIndex(el);
+            this.options.filters[index].attr("operator", el.val());
+        },
+
+        '.filterText keyup': function(el, ev) {
+            if(ev.keyCode == 13) {
+                masterDataTable.fnStandingRedraw();
+            } else {
+                var index = this.findIndex(el);
+                this.options.filters[index].attr("text", el.val());
             }
-        });
+        },
 
-        masterDataTable.fnDraw();
+        '.deleteFilter click': function(el, ev) {
+            var index = this.findIndex(el);
+            this.options.filters.splice(index, 1);
+            $(".filterSelect").ufd();
+            masterDataTable.fnStandingRedraw();
+        },
+
+        findIndex: function(el) {
+            return $('.filterItem').index(el.closest('.filterItem'));
+        }
+    });
+
+    AndVOrControl  = can.Control({
+        init: function(element, options) {
+            this.element.html(can.view('static/data_app/views/andVor.ejs', {
+                andVor: this.options.andVor
+            }));
+        },
+
+        '#andVor change': function(el, ev) {
+            this.options.andVor.attr("andVor", el.val());
+        }
+    });
+
+    ColumnsControl = can.Control({
+        // Make sure all of the columns are in the correct order, with correct visibility
+        init: function(element, options) {
+            columns = this.options.columns;
+
+            var oSettings = masterDataTable.fnSettings();
+
+            calculateIndexNums();
+
+            // Goes through and puts all of the visible columns in the correct
+            // order on the left, and the invisible columns on the right.
+            for(i = 0; i < columns.length; i++) {
+                var from = indexNums[columns[i]];
+                var to = i;
+                toTitle = oSettings.aoColumns[to].sTitle;
+
+                if(from !== to) {
+                    masterDataTable.fnColReorder(from, to);
+
+
+                    // Updates our cache of column order
+                    var min = (to < from)? to : from;
+                    var max = (to > from)? to : from;
+                    for(j = min; j <= max; j++) {
+                        indexNums[oSettings.aoColumns[j].sTitle] = j;
+                    }
+                }
+            }
+
+            // Hide the columns that aren't mentioned in the column list
+            for(i = 0; i < oSettings.aoColumns.length; i++) {
+                masterDataTable.fnSetColumnVis(i, (i < columns.length));
+            }
+
+            masterDataTable.bind('column-reorder', this.updateColumns);
+        },
+
+        updateColumns: function() {
+            var oSettings = masterDataTable.fnSettings();
+            var aoColumns = oSettings.aoColumns;
+            var size = aoColumns.length;
+
+            var newColumns = [];
+            for(i = 0; i < size; i++) {
+                if(aoColumns[i].bVisible) {
+                    newColumns.push(aoColumns[i].sTitle);
+                }
+            }
+
+            columns.attr(newColumns);
+        }
+    });
+
+    VisControl = can.Control({
+        init: function() {
+            this.element.html(can.view('static/data_app/views/visDialog.ejs', {
+                columns: masterDataTable.fnSettings().aoColumns
+            }));
+
+            $("#visDialog").dialog({
+                autoOpen: false,
+                buttons: {
+                    "Ok": function() {
+                        updateColumnVisibility();
+                        $(this).dialog("close");
+                    }
+                },
+                modal: true,
+                draggable: false,
+                minWidth: 500,
+                title: "Show / Hide Columns",
+                show: { effect: "fade", speed: 1000 },
+                hide: { effect: "fade", speed: 1000 }
+            });
+        },
+
+        '#visDialogButton click': function() {
+            $("#visDialog").dialog('open');
+        },
+
+        '#visDialog close': function() {
+            calculateIndexNums();
+            $(".colVisCheckbox").each(function() {
+                masterDataTable.fnSetColumnVis(indexNums[$(this).attr("name")], $(this).attr("checked") === "checked");
+            });
+        }
+    });
+
+    var workspace = {
+        "andVor": "and",
+        "filters": [
+            {
+                "not": false,
+                "selection": "Lab 1 Density",
+                "operator": "<=",
+                "text": "40"
+            },
+            {
+                "not": true,
+                "selection": "Final Exam",
+                "operator": ">",
+                "text": "200"
+            }
+        ],
+        "columns": [
+            "lastname",
+            "firstname",
+            "netid",
+            "email",
+            "Lab 1 Density",
+            "Final Exam"
+        ],
+        "sorting": {
+            "column": "lastname",
+            "direction": "asc"
+        }
+    };
+
+    var filters = new can.Observe.List(workspace.filters);
+    var andVor = new can.Observe().attr("andVor", workspace.andVor);
+    var columns = new can.Observe.List(workspace.columns);
+
+    new FiltersControl('#filterDivContainer', {
+        filters: filters
+    });
+
+    new AndVOrControl('#andVorDiv', {
+        andVor: andVor
+    });
+
+    new ColumnsControl('#userTable', {
+        columns: columns
+    });
+
+    new VisControl('#visDialogContainer');
+
+    $("#newFilter").bind("click", function() {
+        filters.push({not: false, "selection": "", operator: "<", "text": ""});
+        $(".filterSelect").ufd();
+    });
+
+    // Restore sorting order from workspace
+    calculateIndexNums();
+    var settings = masterDataTable.fnSettings();
+    settings.aaSorting[0][0] = indexNums[workspace.sorting.column];
+    settings.aaSorting[0][1] = workspace.sorting.direction;
+    settings.aaSorting[0][2] = workspace.sorting.direction === "asc"? 0 : 1;
+
+    masterDataTable.bind('sort', function() {
+        var sortData = settings.aaSorting[0];
+        if(workspace.sorting.column !== settings.aoColumns[sortData[0]].sTitle ||
+                workspace.sorting.direction !== sortData[2]) {
+            workspace.sorting.column = settings.aoColumns[sortData[0]].sTitle;
+            workspace.sorting.direction = sortData[2];
+        }
+    });
+
+    masterDataTable.fnDraw();
+}
+
+$(document).ready(function() {
+    $.ajax( {
+        "dataType": "text",
+        "type": "GET",
+        "url": "/static/data_app/formatted.txt",
+        "success": function(dataStr) {
+            var data = eval('(' + dataStr + ')');
+
+            masterDataTable = $("#userTable").dataTable({
+                "bJQueryUI": true,
+                "bProcessing": true,
+                "sDom": 'R<"H"lr>t<"F"ip>',
+                "bScrollXInner": true,
+                "sScrollX": "100%",
+                "bScrollAutoCss": true,
+                "bDeferRender": true,
+
+                "aaData": data.aaData,
+                "aoColumns": data.aoColumns
+            });
+
+            tableInitialized();
+        }
     });
 });
 
